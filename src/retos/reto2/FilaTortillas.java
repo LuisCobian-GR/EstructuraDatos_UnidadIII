e4f1b97cc422ba7c11b60babf2e5b244c8218375
Luis Cobian <coby10@gmail.com>
2025-10-22 09:52:15 -0600

    Cambios con refactorizacion a recursividad
    Metodos recursivos

--- src/retos/reto2/FilaTortillas.java
+++ src/retos/reto2/FilaTortillas.java
@@ -22,58 +22,65 @@ public class FilaTortillas {
             System.out.println("4. Terminar servicio");
             opc = Integer.parseInt(sc.nextLine()); 
             switch(opc){
-                case 1: 
+                case 1 -> { 
                     System.out.println("Dame un nombre:");
-                    String nombre = sc.nextLine(); 
+                    String nombre = sc.nextLine();
                     Persona personaNueva = new Persona(); 
                     personaNueva.nombre = nombre;
                     if(inicioFila == null)
-                        inicioFila = personaNueva; 
-                    else {
-                        Persona siguiente = inicioFila;
-                        while(siguiente.vieneAtras != null){
-                            siguiente = siguiente.vieneAtras; 
-                        }
-                        siguiente.vieneAtras =personaNueva; 
-                    }
-                    imprimirLista(inicioFila);
-                    break; 
-                case 2: 
+                        inicioFila = personaNueva;
+                    else
+                        acomodaPersona(inicioFila, personaNueva); 
+                    imprimirLista(inicioFila,0);
+                }
+                case 2 -> { 
                     if(inicioFila!=null)
                         inicioFila = inicioFila.vieneAtras;
-                    imprimirLista(inicioFila); 
-                    break; 
-                case 3: 
+                    imprimirLista(inicioFila,0);
+                }
+                case 3 -> { 
                     System.out.println("A quien quieres eliminar:");
-                    String elimina = sc.nextLine(); 
-                    Persona buscado = inicioFila; 
-                    Persona atras = null;
-                    while(!buscado.nombre.equals(elimina) 
-                            && buscado!=null){
-                        atras = buscado; 
-                        buscado = buscado.vieneAtras; 
-                    }
-                    //  caso 1 
+                    String elimina = sc.nextLine();
+                    Persona[] buscadoEncontrados = encuentraNodo(inicioFila, elimina, null); 
+                    Persona atras = buscadoEncontrados[1];
+                    Persona buscado = buscadoEncontrados[0];
                     if(buscado!=null){
                         //caso 1 
                         if(atras == null) inicioFila = inicioFila.vieneAtras;
-                        else if(buscado.vieneAtras == null) atras.vieneAtras=null; 
+                        else if(buscado.vieneAtras == null) atras.vieneAtras=null;
                         else atras.vieneAtras = buscado.vieneAtras; 
                     }
-                case 4: 
+                }
+                case 4 -> { 
+                    System.out.println("Terminando servicio");
+                    System.out.println("Personas listado de personas que se quedaron sin servicio");
+                    System.out.println("Numero total: " + imprimirLista(inicioFila, 0));
+                }
             }
         }while(opc!=4);     
     }
-    
-    public static void imprimirLista(Persona persona){
-        if(persona != null){
-            System.out.println(persona.nombre);
-            imprimirLista(persona.vieneAtras);
-        }
+   
+    public static Persona[] encuentraNodo(Persona personaEnFila, String encuentra, Persona personaAnterior ){
+        if( personaEnFila == null || personaEnFila.nombre.equals(encuentra))
+            return new Persona[] {personaEnFila, personaAnterior}; 
+        else 
+            return encuentraNodo(personaEnFila.vieneAtras, encuentra, personaEnFila);
     }
-}
-
-class Persona{
-    String nombre; 
-    Persona vieneAtras; 
+    
+    public static void acomodaPersona(Persona personaEnFila, Persona personaNueva){
+        if(personaEnFila.vieneAtras == null)
+            personaEnFila.vieneAtras = personaNueva;
+        else 
+            acomodaPersona(personaEnFila.vieneAtras, personaNueva); 
+    }
+    
+    
+    public static int imprimirLista(Persona persona, int n){
+        if(persona != null){
+            n++; 
+            System.out.println("Lugar: " + n + " - " + persona.nombre);
+            return imprimirLista(persona.vieneAtras, n);
+        } 
+        return n; 
+    }
 }
